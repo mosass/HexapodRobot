d1359d6bb46716685c700a93e6e4770c04e4d983
Phanomphon Yotchon <mosy.mj@gmail.com>
2017-02-24 00:52:15 +0700

    Adding Vivado project file.

---
+++ VIVADO/hexapod/hexapod.cache/ip/15bdd5a11717248e/design_1_xlconcat_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2016.4 (win64) Build 1733598 Wed Dec 14 22:35:39 MST 2016
+// Date        : Thu Feb 09 23:36:35 2017
+// Host        : TheMosass-PC running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_xlconcat_0_0_stub.v
+// Design      : design_1_xlconcat_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "xlconcat,Vivado 2016.4" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(In0, In1, dout)
+/* synthesis syn_black_box black_box_pad_pin="In0[0:0],In1[0:0],dout[1:0]" */;
+  input [0:0]In0;
+  input [0:0]In1;
+  output [1:0]dout;
+endmodule
