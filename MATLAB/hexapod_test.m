c4f6b9894231c04b0935229116865133e0f07e79
Phanomphon Yotchon <mosy.mj@gmail.com>
2017-05-17 20:56:18 +0700

    Rewrite Hexapod model in matlab

--- MATLAB/hexapod_test.m
+++ MATLAB/hexapod_test.m
@@ -6,32 +6,22 @@ robot = hexapod();
 
 robot.plot();
 
-%       T   x   y   z   r   p   y
-seg = [ 1   0   0   0   0   0   0;
-        1   0   0   4   0   0   0; 
-        1   0   0   -3  0   0   0;
-        1   3   0   -3  0   0   0;
-        1   3   3   -3  0   0   0;
-        1   0   3   -3  0   0   0;
-        1   -3  3   -3  0   0   0;
-        1   -3  0   -3  0   0   0;
-        1   -3  -3  -3  0   0   0;
-        1   0   -3  -3  0   0   0;
-        1   3   -3  -3  0   0   0;
+
+%       T       x       y       z 
+seg = [ 1       0       13.1    0 ;
+        1       0       13.1    3 ;
+        1       3       13.1    3 ;
+        1       3       13.1    0 ;
+        1       0       13.1    0 ; 
       ];
 
 
-traj = mstraj(seg(:,2:7), [], seg(:,1)', seg(1,2:7), 0.3, 0.1);
+traj = mstraj(seg(:,2:4), [], seg(:,1)', seg(1,2:4), 0.01, 0.01);
 
 [M N] = size(traj);
 for i = 1:M
-    robot.setBodyTransl(traj(i, :));
-    robot.animate();
+    robot.animate(traj(i, :));
 end
-
-
-robot.setBodyTransl([0 0 0 5 0 0]);
-robot.animate();
 
 
 
--- MATLAB/matlab_module/hexapod.m
+++ MATLAB/matlab_module/hexapod.m
@@ -7,33 +7,33 @@ classdef hexapod < handle
         FEMUR = 5.6;
         TIBIA = 7.5;
         
-        LEG_BASE_X = 10;
-        LEG_BASE_Y = 5;
-        LEG_BASE_Z = hexapod.TIBIA;
+        LEG_BASE_X = 15.75;
+        LEG_BASE_Y = 4.375;
+        LEG_BASE_Z = 7.5;
+        
+        LEG_INIT_FT = [0 13.1 0];
         
         LEGS_BASE = [
-            hexapod.LEG_BASE_X      -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
-                0                   -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
-            -hexapod.LEG_BASE_X     -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
             hexapod.LEG_BASE_X      hexapod.LEG_BASE_Y    hexapod.LEG_BASE_Z;
                 0                   hexapod.LEG_BASE_Y    hexapod.LEG_BASE_Z;
             -hexapod.LEG_BASE_X     hexapod.LEG_BASE_Y    hexapod.LEG_BASE_Z;
+            hexapod.LEG_BASE_X      -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
+                0                   -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
+            -hexapod.LEG_BASE_X     -hexapod.LEG_BASE_Y   hexapod.LEG_BASE_Z;
         ];
     end
     
     properties
         % Legs joint variable
-        jointsVar;
         footTipsPos;
         
         zoffset;
         
-        baseLegs;
         legs;
     end
     
     methods(Static)
-        function r = leg()
+        function r = leg(z_off)
             L1 = hexapod.COXA;
             L2 = hexapod.FEMUR;
             L3 = hexapod.TIBIA;
@@ -44,103 +44,96 @@ classdef hexapod < handle
             links(3) = Link([    0       0    L3      0 ]);
 
             r = SerialLink(links, 'name', 'leg', ...
-                'base', transl(0, 0, L3), ...
-                'offset', [-pi/2 0 -pi/2]);
+                'base', transl(0, 0, z_off), ...
+                'offset', [pi/2 0 -pi/2]);
         end
         
-        function r = initBody6l()
+        function r = initLegs6l(z_off)
             r_tmp(6,1) = SerialLink;
             for i = 1:6
                 xb = hexapod.LEGS_BASE(i, 1);
                 yb = hexapod.LEGS_BASE(i, 2);
-                zb = hexapod.LEGS_BASE(i, 3);
-                
-                bl = Link([0 0 sqrt(xb^2 + yb^2) 0]);
-                if xb < 0
-                    bl.offset = pi + atan(yb/xb);
-                else
-                    bl.offset = atan(yb/xb);
+                rot = trotz(0);
+                if yb < 0
+                    rot = trotz(pi);
                 end
-                r_tmp(i) = SerialLink(bl, ...
-                    'name', strcat('base', int2str(i)), ...
-                    'base', transl([0 0 zb]));
-                r_tmp(i).qlim(1,:) = [0 0];
-            end
-            r = r_tmp;
-        end
-        
-        function r = initLegs6l()
-            r_tmp(6,1) = SerialLink;
-            for i = 1:6
-                xb = hexapod.LEGS_BASE(i, 1);
-                yb = hexapod.LEGS_BASE(i, 2);
-                offs = atan(xb/yb) + pi/2; 
-                L = hexapod.leg();
+                L = hexapod.leg(z_off);
                 r_tmp(i) = SerialLink(L, ...
-                    'name', strcat('leg', int2str(i)), ...
-                    'offset', L.offset+[offs 0 0]);
+                    'base', transl(xb, yb, z_off)*rot, ...
+                    'name', strcat('leg', int2str(i)));
             end
             r = r_tmp;
         end
         
-        function r = getTransl(tran3d, rot3d)
-            r = transl(tran3d) * trotx(rot3d(1)) ...
-                * troty(rot3d(2)) * trotz(rot3d(3));
+        function r = legIk(ft, z_off)
+            x = ft(1);
+            y = ft(2);
+            z = ft(3);
+            C = hexapod.COXA;
+            F = hexapod.FEMUR;
+            T = hexapod.TIBIA;
+            
+            alpha = atan(x/y);
+            L1 = sqrt(x^2 + y^2);
+            L = sqrt((L1 - C)^2 + (z_off - z)^2);
+            beta = acos((F^2 + L^2 - T^2)/(2*L*F)) - atan((z_off - z)/(L1 - C));
+            gramma = acos((T^2 + F^2 - L^2)/(2*T*F)) - pi/2;
+            
+            r = [alpha beta gramma];
         end
     end
     
 	methods        
         function obj = hexapod()
             obj.zoffset = hexapod.LEG_BASE_Z;
-            obj.jointsVar = zeros(6,3);
-            obj.footTipsPos = zeros(6,3);
+            obj.footTipsPos = repmat(hexapod.LEG_INIT_FT, 6, 1);
+            obj.legs = hexapod.initLegs6l(obj.zoffset);
+        end
+        
+        function r = legIkForLeg(obj, legId)
+            q = hexapod.legIk(obj.footTipsPos(legId,:), obj.zoffset);
             
-            obj.legs = hexapod.initLegs6l();
-            obj.baseLegs = hexapod.initBody6l();
-            for i = 1:6
-                obj.baseLegs(i).base = hexapod.getTransl([0 0 obj.zoffset], [0 0 0]);
-                obj.legs(i).base = obj.baseLegs(i).fkine(0);
-                obj.footTipsPos(i, :) = tform2trvec(obj.legs(i).fkine(obj.jointsVar(i, :)));
+            if legId < 4
+                q(1) = -q(1);
             end
-        end
-        
-        function setBodyTransl(obj, v6d)
-            v6d(3) = v6d(3) + obj.zoffset;
-            v6d(4:6) = deg2rad(v6d(4:6));
-            for i = 1:6
-                obj.baseLegs(i).base = hexapod.getTransl(v6d(1:3), v6d(4:6));
-            end
-            obj.updateLegsBase();
-        end
-        
-        function updateLegsBase(obj)
-            for i = 1:6
-                v_old = tform2trvec(obj.legs(i).fkine(obj.jointsVar(i,:)));
-                obj.legs(i).base = obj.baseLegs(i).fkine(0);
-                obj.jointsVar(i,:) = obj.legs(i).ikine(transl(v_old), [0 0 0], [1 1 1 0 0 0]);
-            end
+            r = q;
         end
         
         function plot(obj)
             plotopt = {'nobase', 'nowrist', 'noname', ...
                        'delay', 0.1, ...
                        'noshading', 'floorlevel', 0};
-            hold on
+            
+            view(3)
             axis([-30 30 -30 30 -5 20]);
+            hold on
+            
+            patch_vert = [0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1];
+            patch_fac = [1 2 6 5;2 3 7 6;3 4 8 7;4 1 5 8;1 2 3 4;5 6 7 8];
+            patch_scale = [hexapod.LEG_BASE_X*2 + 6 ...
+                    hexapod.LEG_BASE_Y*2 ...
+                    hexapod.LEG_BASE_Z*0.5];
+            patch_move = [-hexapod.LEG_BASE_X - 3 ...
+                    -hexapod.LEG_BASE_Y ...
+                    hexapod.LEG_BASE_Z*0.75];
+            
+            patch_vert = (patch_vert .* repmat(patch_scale,8,1)) + repmat(patch_move,8,1);
+            
+            patch('Vertices', patch_vert, 'Faces', patch_fac, 'FaceVertexCData', [1:6]', 'FaceColor', 'flat');
             for i = 1:6
-                obj.legs(i).plot(obj.jointsVar(i,:), plotopt{:});
-                obj.baseLegs(i).plot(0, plotopt{:});
+                q = obj.legIkForLeg(i);
+                obj.legs(i).plot(q, plotopt{:});
             end
             hold off
         end
         
-        function animate(obj)
+        function animate(obj, ft)
             for i = 1:6
-                obj.legs(i).animate(obj.jointsVar(i,:));
-                obj.baseLegs(i).animate(0);
+                obj.footTipsPos(i,:) = ft;
+                q = obj.legIkForLeg(i)
+                obj.legs(i).animate(q);
             end
         end
-   
     end
 end
 
